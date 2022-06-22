f5e00ee28acad1c5f59fcc1437bd97dc28c3e498
Dorota638 <1081600@ucn.dk>
2022-06-22 20:03:33 +0200

    bicycle form

--- web/src/components/repair/CreateBicycle.js
+++ web/src/components/repair/CreateBicycle.js
@@ -2,15 +2,15 @@ import { Modal } from '@mantine/core';
 import React from 'react';
 import NewBicycle from '../forms/NewBicycleForm';
 
-const CreateBicycle = ({opened, setOpened}) => {
-  
+const CreateBicycle = ({ opened, setOpened }) => {
+
   return (
     <Modal
       opened={opened}
       onClose={() => setOpened(false)}
-      title="Create customer"
+      title="Create bicycle"
     >
-      <NewBicycle setOpened={setOpened}/>
+      <NewBicycle setOpened={setOpened} />
     </Modal>
   );
 };
