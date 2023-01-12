eccabcdc82bbca84e6c7feb5757cb84d44a4f9b6
Matus Laco <matuslaco@Matuss-Laptop.local>
2023-01-12 14:42:04 +0100

    docker setup

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize('flexidb', 'root', 'root', {
-  host: '185.139.228.227',
+  host: 'localhost',
   port: 3306,
   dialect: 'mysql',
 });
