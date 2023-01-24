14c5f629e97e0cbd3dc6535d0ed61ba07d90545b
Matus Laco <laco.matus@gmail.com>
2023-01-24 23:37:15 +0100

    ssl

--- server/src/graphql/query/products.ts
+++ server/src/graphql/query/products.ts
@@ -1,92 +1,99 @@
-import { Op } from 'sequelize';
-import { errHandler } from '../../helper';
+import { Op, Sequelize } from "sequelize";
+import { errHandler } from "../../helper";
 const {
-  Product,
-  ProductSupplier,
-  ProductBrand,
-  ProductCategory,
-  ProductGroup,
-} = require('../../models/Product');
+	Product,
+	ProductSupplier,
+	ProductBrand,
+	ProductCategory,
+	ProductGroup,
+} = require("../../models/Product");
 
 export const queryResolvers = {
-  async products() {
-    try {
-      const products = await Product.findAll().catch(errHandler);
-      // console.log(
-      //   'products',
-      //   products.map((product) => product.dataValues.fkGroup)
-      // );
+	async products() {
+		try {
+			const products = await Product.findAll().catch(errHandler);
+			// console.log(
+			//   'products',
+			//   products.map((product) => product.dataValues.fkGroup)
+			// );
 
-      return products;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-  async productsByCategory(_: any, input: { categoryId: string }) {
-    try {
-      const products = await Product.findAll({
-        where: { fkCategory: input.categoryId },
-      }).catch(errHandler);
-      return products;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-  async productsByName(_: any,  input: { name: string }) {
-    try {
-      const products = await Product.findAll({
-        where: {
-          description: {
-            [Op.startsWith]: input.name[0],
-          },
-        },
-      }).catch(errHandler);
-      return products;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
+			return products;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	async productsByCategory(_: any, input: { categoryId: string }) {
+		try {
+			const products = await Product.findAll({
+				where: { fkCategory: input.categoryId },
+			}).catch(errHandler);
+			return products;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	async productsByName(_: any, input: { name: string }) {
+		try {
+			const products = await Product.findAll({
+				where: {
+					[Op.or]: [
+						{ description: { [Op.substring]: input.name } },
+						{ ean: { [Op.substring]: input.name } },
+						{
+							fkBrand: {
+								[Op.in]: Sequelize.literal(
+									`(SELECT id FROM ProductBrands WHERE value LIKE '%${input.name}%')`
+								),
+							},
+						},
+						{
+							fkCategory: {
+								[Op.in]: Sequelize.literal(
+									`(SELECT id FROM ProductCategories WHERE value LIKE '%${input.name}%')`
+								),
+							},
+						},
+					],
+				},
+			}).catch(errHandler);
+			return products;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
 };
 
 export const resolvers = {
-  productSupplier: async (parent: any) => {
-    try {
-      const supplier = await ProductSupplier.findByPk(parent.fkSupplier).catch(
-        errHandler
-      );
-      return supplier;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-  productBrand: async (parent: any) => {
-    try {
-      const brand = await ProductBrand.findByPk(parent.fkBrand).catch(
-        errHandler
-      );
-      return brand;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-  productCategory: async (parent: any) => {
-    try {
-      const category = await ProductCategory.findByPk(parent.fkCategory).catch(
-        errHandler
-      );
-      return category;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
-  productGroup: async (parent: any) => {
-    try {
-      const group = await ProductGroup.findByPk(parent.fkGroup).catch(
-        errHandler
-      );
-      return group;
-    } catch (err) {
-      throw new Error(err);
-    }
-  },
+	productSupplier: async (parent: any) => {
+		try {
+			const supplier = await ProductSupplier.findByPk(parent.fkSupplier).catch(errHandler);
+			return supplier;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	productBrand: async (parent: any) => {
+		try {
+			const brand = await ProductBrand.findByPk(parent.fkBrand).catch(errHandler);
+			return brand;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	productCategory: async (parent: any) => {
+		try {
+			const category = await ProductCategory.findByPk(parent.fkCategory).catch(errHandler);
+			return category;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
+	productGroup: async (parent: any) => {
+		try {
+			const group = await ProductGroup.findByPk(parent.fkGroup).catch(errHandler);
+			return group;
+		} catch (err) {
+			throw new Error(err);
+		}
+	},
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -14,10 +14,10 @@ const app = express();
 const PORT = 3000;
 const configurations: any = {
 	// Note: You may need sudo to run on port 443
-	production: { ssl: true, port: 443, hostname: "https://kalinovskyklin.xyz" },
-	development: { ssl: false, port: 3000, hostname: "localhost" },
+	production: { ssl: true, port: 443, hostname: "kalinovskyklin.xyz" },
+	development: { ssl: true, port: 3000, hostname: "localhost" },
 };
-const environment = process.env.NODE_ENV || "production";
+const environment = process.env.NODE_ENV || "development";
 const config = configurations[environment];
 
 const main = async () => {
@@ -40,7 +40,7 @@ const main = async () => {
 
 	app.use(
 		"/graphql",
-		cors({ origin: ["https://kalinovskyklin.xyz", "http://kalinovskyklin.xyz", "http://localhost:3001"] }),
+		cors(),
 		json(),
 		expressMiddleware(server, {
 			context: async ({ req }) => ({ token: req.headers.token }),
